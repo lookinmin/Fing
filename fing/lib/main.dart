694969f936a82b1c1a4373f582807ad31baf9896
kkm0406 <sdjmc30412@naver.com>
2022-07-21 19:36:50 +0900

    Fing start

--- fing/lib/main.dart
+++ fing/lib/main.dart
@@ -21,8 +21,78 @@ class MainPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text("Fing")),
-      body: const Center(child: Text('MAIN')),
+      body: AddressSearch(),
+      bottomNavigationBar: BottomAppBar(
+        child: BottomMenu(),
+      ),
     ); // TODO: implement build
+  }
+}
+
+class BottomMenu extends StatelessWidget {
+  const BottomMenu({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: 65,
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: [
+          Container(
+            padding: EdgeInsets.fromLTRB(0, 10, 0, 10),
+            child: Column(children: [Icon(Icons.home), Text('홈')]),
+          ),
+          Container(
+            padding: EdgeInsets.fromLTRB(0, 10, 0, 10),
+            child: Column(children: [Icon(Icons.map_outlined), Text('지도')]),
+          ),
+          Container(
+            padding: EdgeInsets.fromLTRB(0, 10, 0, 10),
+            child: Column(children: [Icon(Icons.assignment), Text('게시판')]),
+          ),
+          Container(
+              padding: EdgeInsets.fromLTRB(0, 10, 0, 10),
+              child: Column(children: [Icon(Icons.person), Text('내 정보')]))
+        ],
+      ),
+    );
+    ;
+  }
+}
+
+class AddressSearch extends StatelessWidget {
+  const AddressSearch({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.fromLTRB(30, 10, 30, 20),
+      padding: EdgeInsets.fromLTRB(20, 10, 20, 10),
+      decoration: BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.circular(15),
+          boxShadow: [
+            BoxShadow(
+              color: Colors.grey.withOpacity(0.4),
+              spreadRadius: 0,
+              blurRadius: 2,
+              offset: Offset(0, 7), // changes position of shadow
+            )
+          ]),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Text(
+            '검색할 주소를 입력하세요',
+            style: TextStyle(color: Colors.grey, fontWeight: FontWeight.w700),
+          ),
+          Icon(
+            Icons.search,
+            color: Color.fromRGBO(248, 180, 0, 1.0),
+          )
+        ],
+      ),
+    );
   }
 }
