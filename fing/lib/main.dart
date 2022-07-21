d1373520f71831bf2c5a1dfbb92b90fb531bb979
MINSU <ancx1234@naver.com>
2022-07-21 21:18:03 +0900

    민수/main/Update 하단바

--- fing/lib/main.dart
+++ fing/lib/main.dart
@@ -41,78 +41,118 @@ class _MyWidgetState extends State<MyWidget> {
   var black = Color.fromARGB(255, 0, 0, 0);
   List<bool> isClick = [true, true, true, true, true];
   Widget build(BuildContext context) {
-    return SizedBox(
-      height: 70,
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-        children: [
-          Column(children: [
-            IconButton(
-                onPressed: () {
-                  setState(() {
-                    isClick[0] = !isClick[0];
-                  });
-                },
-                icon: Icon(Icons.home_outlined),
-                color: isClick[0] ? black : orange),
-            Text('  홈  ', style: TextStyle(color: isClick[0] ? black : orange))
-          ]),
-          Column(children: [
-            IconButton(
-                onPressed: () {
-                  setState(() {
-                    isClick[1] = !isClick[1];
-                  });
-                },
-                icon: Icon(Icons.place_outlined),
-                color: isClick[1] ? black : orange),
-            Text(' 지역 ', style: TextStyle(color: isClick[1] ? black : orange))
-          ]),
-          Column(children: [
-            IconButton(
-                onPressed: () {
-                  setState(() {
-                    isClick[2] = !isClick[2];
-                  });
-                },
-                icon: Icon(Icons.map_outlined),
-                color: isClick[2] ? black : orange),
-            Text(' 내주변 ', style: TextStyle(color: isClick[2] ? black : orange))
-          ]),
-          Column(children: [
-            IconButton(
-                onPressed: () {
-                  setState(() {
-                    isClick[3] = !isClick[3];
-                  });
-                },
-                icon: Icon(Icons.favorite_outline),
-                color: isClick[3] ? black : orange),
-            Text(
-              '  찜  ',
-              style: TextStyle(color: isClick[3] ? black : orange),
-            )
-          ]),
-          Column(children: [
-            IconButton(
-                onPressed: () {
-                  setState(() {
-                    isClick[4] = !isClick[4];
-                  });
-                },
-                icon: Icon(Icons.person_outline),
-                color: isClick[4] ? black : orange),
-            Text(
-              '마이페이지',
-              style: TextStyle(color: isClick[4] ? black : orange),
-            )
-          ]),
-        ],
+    return Container(
+      padding: EdgeInsets.fromLTRB(10, 10, 10, 10),
+      child: SizedBox(
+        height: 60,
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            Column(children: [
+              IconButton(
+                  onPressed: () {
+                    setState(() {
+                      isClick[1] = true;
+                      isClick[2] = true;
+                      isClick[3] = true;
+                      isClick[4] = true;
+                      isClick[0] = !isClick[0];
+                    });
+                  },
+                  icon: Icon(Icons.home_outlined),
+                  color: isClick[0] ? black : orange),
+              Text('  홈  ',
+                  style: TextStyle(
+                      color: isClick[1] ? black : orange,
+                      fontSize: 11,
+                      fontWeight: FontWeight.w700))
+            ]),
+            Column(children: [
+              IconButton(
+                  onPressed: () {
+                    setState(() {
+                      isClick[0] = true;
+                      isClick[2] = true;
+                      isClick[3] = true;
+                      isClick[4] = true;
+                      isClick[1] = !isClick[1];
+                    });
+                  },
+                  icon: Icon(Icons.place_outlined),
+                  color: isClick[1] ? black : orange),
+              Text(' 지역 ',
+                  style: TextStyle(
+                      color: isClick[1] ? black : orange,
+                      fontSize: 11,
+                      fontWeight: FontWeight.w700))
+            ]),
+            Column(children: [
+              IconButton(
+                  onPressed: () {
+                    setState(() {
+                      isClick[1] = true;
+                      isClick[0] = true;
+                      isClick[3] = true;
+                      isClick[4] = true;
+                      isClick[2] = !isClick[2];
+                    });
+                  },
+                  icon: Icon(Icons.map_outlined),
+                  color: isClick[2] ? black : orange),
+              Text(' 내주변 ',
+                  style: TextStyle(
+                      color: isClick[1] ? black : orange,
+                      fontSize: 11,
+                      fontWeight: FontWeight.w700))
+            ]),
+            Column(children: [
+              IconButton(
+                  onPressed: () {
+                    setState(() {
+                      isClick[1] = true;
+                      isClick[2] = true;
+                      isClick[0] = true;
+                      isClick[4] = true;
+                      isClick[3] = !isClick[3];
+                    });
+                  },
+                  icon: Icon(Icons.favorite_outline),
+                  color: isClick[3] ? black : orange),
+              Text(
+                '  찜  ',
+                style: TextStyle(
+                    color: isClick[1] ? black : orange,
+                    fontSize: 11,
+                    fontWeight: FontWeight.w700),
+              )
+            ]),
+            Column(children: [
+              IconButton(
+                  onPressed: () {
+                    setState(() {
+                      isClick[1] = true;
+                      isClick[2] = true;
+                      isClick[3] = true;
+                      isClick[0] = true;
+                      isClick[4] = !isClick[4];
+                    });
+                  },
+                  icon: Icon(Icons.person_outline),
+                  color: isClick[4] ? black : orange),
+              Text(
+                '마이페이지',
+                style: TextStyle(
+                    color: isClick[1] ? black : orange,
+                    fontSize: 11,
+                    fontWeight: FontWeight.w700),
+              )
+            ]),
+          ],
+        ),
       ),
     );
   }
 }
-
 // class BottomMenu extends StatelessWidget {
 //   const BottomMenu({Key? key}) : super(key: key);
 //   var orange = const Color.fromARGB(
