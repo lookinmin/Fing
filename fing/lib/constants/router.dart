8fbf23a7559661052916ad8bdc43b0d8297bf1c6
MINSU <ancx1234@naver.com>
2022-07-25 18:37:34 +0900

    민수/main/Router 구성

---
+++ fing/lib/constants/router.dart
@@ -0,0 +1,6 @@
+const String home = "/";
+const String goods = "/goods";
+const String local = "/local";
+const String around = "/around";
+const String like = "/like";
+const String myInfo = "/myInfo";
