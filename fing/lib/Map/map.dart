41c1739274812b025749c0bc16ae69f8d36047d1
kkm0406 <sdjmc30412@naver.com>
2022-07-27 21:55:20 +0900

    feature-map/광모/카카오맵 연동

---
+++ fing/lib/Map/map.dart
@@ -0,0 +1,100 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_kakao_map/flutter_kakao_map.dart';
+import 'package:flutter_kakao_map/kakao_maps_flutter_platform_interface.dart';
+// import 'package:kakao_flutter_sdk/kakao_flutter_sdk_common.dart';
+
+void main() {
+  AddressMap();
+}
+
+class AddressMap extends StatefulWidget {
+  const AddressMap({Key? key}) : super(key: key);
+
+  @override
+  State<AddressMap> createState() => _AddressMapState();
+}
+
+class _AddressMapState extends State<AddressMap> {
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        // Flexible(flex: 1, fit: FlexFit.tight, child: NowAddressText()),
+        Expanded(flex: 12, child: NowAddressMap())
+      ],
+    );
+  }
+}
+
+class NowAddressMap extends StatefulWidget {
+  NowAddressMap({Key? key}) : super(key: key);
+
+  @override
+  State<NowAddressMap> createState() => _NowAddressMapState();
+}
+
+class _NowAddressMapState extends State<NowAddressMap> {
+  late KakaoMapController mapController;
+  MapPoint _init = MapPoint(37.508, 127.06);
+  CameraPosition _initPosition =
+      CameraPosition(target: MapPoint(37.508, 127.06), zoom: 5);
+
+  void onMapCreated(KakaoMapController controller) async {
+    final MapPoint visibleRegion = await controller.getMapCenterPoint();
+    setState(() {
+      mapController = controller;
+      _init = visibleRegion;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      width: 200,
+      height: 100,
+      child: KakaoMap(
+        onMapCreated: onMapCreated,
+        initialCameraPosition: _initPosition,
+      ),
+    );
+  }
+}
+
+class NowAddressText extends StatefulWidget {
+  const NowAddressText({Key? key}) : super(key: key);
+
+  @override
+  State<NowAddressText> createState() => _NowAddressTextState();
+}
+
+class _NowAddressTextState extends State<NowAddressText> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.symmetric(vertical: 10),
+      padding: EdgeInsets.only(left: 5),
+      child: Row(
+        children: [
+          Container(
+            margin: EdgeInsets.only(right: 5),
+            child: Expanded(
+                child: Icon(
+              Icons.location_on_outlined,
+              size: 30,
+            )),
+          ),
+          Flexible(
+              flex: 5,
+              child: Text(
+                '현재 위치  :  ',
+                style: TextStyle(fontSize: 15, fontWeight: FontWeight.w900),
+              )),
+          Flexible(
+              flex: 12,
+              child: Text('충청북도 청주시 서원구 충대로1',
+                  style: TextStyle(fontSize: 15, fontWeight: FontWeight.w900)))
+        ],
+      ),
+    );
+  }
+}
--- fing/lib/main.dart
+++ fing/lib/main.dart
@@ -1,283 +1,288 @@
-import 'package:flutter/material.dart';
+// import 'package:flutter/material.dart';
+// import 'Map/map.dart';
 
-void main() {
-  runApp(const MyApp());
-}
+// void main() {
+//   runApp(const MyApp());
+// }
 
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+// class MyApp extends StatelessWidget {
+//   const MyApp({Key? key}) : super(key: key);
 
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: "FING",
-      home: MainPage(),
-    );
-  }
-}
+//   // This widget is the root of your application.
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       debugShowCheckedModeBanner: false,
+//       title: "FING",
+//       home: MainPage(),
+//     );
+//   }
+// }
 
-class MainPage extends StatelessWidget {
-  const MainPage({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: Column(
-        children: [
-          Flexible(flex: 2, child: FestivalSearch()),
-          Expanded(child: Text("feature-goods"))
-        ],
-      ),
-      bottomNavigationBar: BottomAppBar(
-        child: BottomMenu(),
-      ),
-    ); // TODO: implement build
-  }
-}
-
-class BottomMenu extends StatefulWidget {
-  const BottomMenu({Key? key}) : super(key: key);
-
-  @override
-  State<BottomMenu> createState() => _BottomMenu();
-}
-
-class _BottomMenu extends State<BottomMenu> {
-  var orange = Color.fromARGB(255, 255, 126, 0);
-  var black = Color.fromARGB(255, 0, 0, 0);
-  List<bool> isClick = [true, true, true, true, true];
-  Widget build(BuildContext context) {
-    return Container(
-      padding: EdgeInsets.fromLTRB(10, 10, 10, 10),
-      child: SizedBox(
-        height: 60,
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          children: [
-            Column(children: [
-              IconButton(
-                  onPressed: () {
-                    setState(() {
-                      isClick[1] = true;
-                      isClick[2] = true;
-                      isClick[3] = true;
-                      isClick[4] = true;
-                      isClick[0] = !isClick[0];
-                    });
-                  },
-                  icon: Icon(Icons.home_outlined),
-                  color: isClick[0] ? black : orange),
-              Text('  홈  ',
-                  style: TextStyle(
-                      color: isClick[0] ? black : orange,
-                      fontSize: 11,
-                      fontWeight: FontWeight.w700))
-            ]),
-            Column(children: [
-              IconButton(
-                  onPressed: () {
-                    setState(() {
-                      isClick[0] = true;
-                      isClick[2] = true;
-                      isClick[3] = true;
-                      isClick[4] = true;
-                      isClick[1] = !isClick[1];
-                    });
-                  },
-                  icon: Icon(Icons.place_outlined),
-                  color: isClick[1] ? black : orange),
-              Text(' 지역 ',
-                  style: TextStyle(
-                      color: isClick[1] ? black : orange,
-                      fontSize: 11,
-                      fontWeight: FontWeight.w700))
-            ]),
-            Column(children: [
-              IconButton(
-                  onPressed: () {
-                    setState(() {
-                      isClick[1] = true;
-                      isClick[0] = true;
-                      isClick[3] = true;
-                      isClick[4] = true;
-                      isClick[2] = !isClick[2];
-                    });
-                  },
-                  icon: Icon(Icons.map_outlined),
-                  color: isClick[2] ? black : orange),
-              Text(' 내주변 ',
-                  style: TextStyle(
-                      color: isClick[2] ? black : orange,
-                      fontSize: 11,
-                      fontWeight: FontWeight.w700))
-            ]),
-            Column(children: [
-              IconButton(
-                  onPressed: () {
-                    setState(() {
-                      isClick[1] = true;
-                      isClick[2] = true;
-                      isClick[0] = true;
-                      isClick[4] = true;
-                      isClick[3] = !isClick[3];
-                    });
-                  },
-                  icon: Icon(Icons.favorite_outline),
-                  color: isClick[3] ? black : orange),
-              Text(
-                '  찜  ',
-                style: TextStyle(
-                    color: isClick[3] ? black : orange,
-                    fontSize: 11,
-                    fontWeight: FontWeight.w700),
-              )
-            ]),
-            Column(children: [
-              IconButton(
-                  onPressed: () {
-                    setState(() {
-                      isClick[1] = true;
-                      isClick[2] = true;
-                      isClick[3] = true;
-                      isClick[0] = true;
-                      isClick[4] = !isClick[4];
-                    });
-                  },
-                  icon: Icon(Icons.person_outline),
-                  color: isClick[4] ? black : orange),
-              Text(
-                '마이페이지',
-                style: TextStyle(
-                    color: isClick[4] ? black : orange,
-                    fontSize: 11,
-                    fontWeight: FontWeight.w700),
-              )
-            ]),
-          ],
-        ),
-      ),
-    );
-  }
-}
-
-// class BottomMenu extends StatelessWidget {
-//   const BottomMenu({Key? key}) : super(key: key);
-//   var orange = const Color.fromARGB(
-//     255,
-//     255,
-//     126,
-//     0,
-//   );
+// class MainPage extends StatelessWidget {
+//   const MainPage({Key? key}) : super(key: key);
 
 //   @override
 //   Widget build(BuildContext context) {
-//     return SizedBox(
-//       height: 70,
-//       child: Row(
-//         mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+//     return Scaffold(
+//       body: Column(
 //         children: [
-//           Column(
-//             children: [
-//               IconButton(onPressed: () {}, icon: Icon(Icons.home_outlined)),
-//               Text('  홈  ')
-//             ],
-//           ),
-//           Column(children: [
-//             IconButton(onPressed: () {}, icon: Icon(Icons.place_outlined)),
-//             Text(' 지역 ')
-//           ]),
-//           Column(children: [
-//             IconButton(onPressed: () {}, icon: Icon(Icons.map_outlined)),
-//             Text(' 내주변 ')
-//           ]),
-//           Column(children: [
-//             IconButton(
-//               onPressed: () {},
-//               icon: Icon(Icons.favorite_outline),
-//               color: Color.fromARGB(
-//                 255,
-//                 255,
-//                 126,
-//                 0,
+//           Flexible(flex: 2, child: FestivalSearch()),
+//           Expanded(flex: 8, child: AddressMap())
+//         ],
+//       ),
+//       // bottomNavigationBar: BottomAppBar(
+//       //   child: BottomMenu(),
+//       // ),
+//     ); // TODO: implement build
+//   }
+// }
+
+// class BottomMenu extends StatefulWidget {
+//   const BottomMenu({Key? key}) : super(key: key);
+
+//   @override
+//   State<BottomMenu> createState() => _BottomMenu();
+// }
+
+// class _BottomMenu extends State<BottomMenu> {
+//   var orange = Color.fromARGB(255, 255, 126, 0);
+//   var black = Color.fromARGB(255, 0, 0, 0);
+//   List<bool> isClick = [true, true, true, true, true];
+//   Widget build(BuildContext context) {
+//     return Container(
+//       padding: EdgeInsets.fromLTRB(10, 10, 10, 10),
+//       child: SizedBox(
+//         height: 60,
+//         child: Row(
+//           mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+//           children: [
+//             Column(children: [
+//               IconButton(
+//                   onPressed: () {
+//                     setState(() {
+//                       isClick[1] = true;
+//                       isClick[2] = true;
+//                       isClick[3] = true;
+//                       isClick[4] = true;
+//                       isClick[0] = !isClick[0];
+//                     });
+//                   },
+//                   icon: Icon(Icons.home_outlined),
+//                   color: isClick[0] ? black : orange),
+//               Text('  홈  ',
+//                   style: TextStyle(
+//                       color: isClick[0] ? black : orange,
+//                       fontSize: 11,
+//                       fontWeight: FontWeight.w700))
+//             ]),
+//             Column(children: [
+//               IconButton(
+//                   onPressed: () {
+//                     setState(() {
+//                       isClick[0] = true;
+//                       isClick[2] = true;
+//                       isClick[3] = true;
+//                       isClick[4] = true;
+//                       isClick[1] = !isClick[1];
+//                     });
+//                   },
+//                   icon: Icon(Icons.place_outlined),
+//                   color: isClick[1] ? black : orange),
+//               Text(' 지역 ',
+//                   style: TextStyle(
+//                       color: isClick[1] ? black : orange,
+//                       fontSize: 11,
+//                       fontWeight: FontWeight.w700))
+//             ]),
+//             Column(children: [
+//               IconButton(
+//                   onPressed: () {
+//                     setState(() {
+//                       isClick[1] = true;
+//                       isClick[0] = true;
+//                       isClick[3] = true;
+//                       isClick[4] = true;
+//                       isClick[2] = !isClick[2];
+//                     });
+//                   },
+//                   icon: Icon(Icons.map_outlined),
+//                   color: isClick[2] ? black : orange),
+//               Text(' 내주변 ',
+//                   style: TextStyle(
+//                       color: isClick[2] ? black : orange,
+//                       fontSize: 11,
+//                       fontWeight: FontWeight.w700))
+//             ]),
+//             Column(children: [
+//               IconButton(
+//                   onPressed: () {
+//                     setState(() {
+//                       isClick[1] = true;
+//                       isClick[2] = true;
+//                       isClick[0] = true;
+//                       isClick[4] = true;
+//                       isClick[3] = !isClick[3];
+//                     });
+//                   },
+//                   icon: Icon(Icons.favorite_outline),
+//                   color: isClick[3] ? black : orange),
+//               Text(
+//                 '  찜  ',
+//                 style: TextStyle(
+//                     color: isClick[3] ? black : orange,
+//                     fontSize: 11,
+//                     fontWeight: FontWeight.w700),
+//               )
+//             ]),
+//             Column(children: [
+//               IconButton(
+//                   onPressed: () {
+//                     setState(() {
+//                       isClick[1] = true;
+//                       isClick[2] = true;
+//                       isClick[3] = true;
+//                       isClick[0] = true;
+//                       isClick[4] = !isClick[4];
+//                     });
+//                   },
+//                   icon: Icon(Icons.person_outline),
+//                   color: isClick[4] ? black : orange),
+//               Text(
+//                 '마이페이지',
+//                 style: TextStyle(
+//                     color: isClick[4] ? black : orange,
+//                     fontSize: 11,
+//                     fontWeight: FontWeight.w700),
+//               )
+//             ]),
+//           ],
+//         ),
+//       ),
+//     );
+//   }
+// }
+
+// class FestivalSearch extends StatelessWidget {
+//   const FestivalSearch({Key? key}) : super(key: key);
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return Container(
+//       height: 80,
+//       decoration: BoxDecoration(
+//           border: Border(
+//               bottom: BorderSide(
+//         color: Colors.grey,
+//         width: 2.0,
+//       ))),
+//       child: Row(
+//         mainAxisAlignment: MainAxisAlignment.start,
+//         children: [
+//           Expanded(flex: 1, child: logo()),
+//           Expanded(
+//             flex: 5,
+//             child: Container(
+//               margin: EdgeInsets.fromLTRB(10, 10, 10, 5),
+//               padding: EdgeInsets.fromLTRB(20, 0, 20, 0),
+//               decoration: BoxDecoration(
+//                   color: Colors.white,
+//                   borderRadius: BorderRadius.circular(15),
+//                   boxShadow: [
+//                     BoxShadow(
+//                       color: Colors.grey.withOpacity(0.4),
+//                       spreadRadius: 0,
+//                       blurRadius: 2,
+//                       offset: Offset(0, 7), // changes position of shadow
+//                     )
+//                   ]),
+//               child: Row(
+//                 mainAxisAlignment: MainAxisAlignment.spaceBetween,
+//                 children: [
+//                   Flexible(
+//                     child: TextField(
+//                       decoration: InputDecoration(
+//                         border: InputBorder.none,
+//                         hintText: 'Search Festival',
+//                       ),
+//                     ),
+//                   ),
+//                   Flexible(
+//                       child: Icon(
+//                     Icons.search,
+//                     color: Color.fromRGBO(255, 126, 0, 1.0),
+//                   ))
+//                 ],
 //               ),
 //             ),
-//             Text(
-//               '  찜  ',
-//               style: TextStyle(color: black),
-//             )
-//           ]),
-//           Column(children: [
-//             IconButton(onPressed: () {}, icon: Icon(Icons.person_outline)),
-//             Text('마이페이지')
-//           ]),
+//           )
 //         ],
 //       ),
 //     );
 //   }
 // }
 
-class FestivalSearch extends StatelessWidget {
-  const FestivalSearch({Key? key}) : super(key: key);
+// Widget logo() {
+//   return Image.asset(
+//     'images/mark.jpg',
+//   );
+// }
 
+// ignore_for_file: import_of_legacy_library_into_null_safe
+
+import 'package:flutter/material.dart';
+import 'package:flutter_kakao_map/flutter_kakao_map.dart';
+import 'package:flutter_kakao_map/kakao_maps_flutter_platform_interface.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      height: 80,
-      decoration: BoxDecoration(
-          border: Border(
-              bottom: BorderSide(
-        color: Colors.grey,
-        width: 2.0,
-      ))),
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.start,
-        children: [
-          Expanded(flex: 1, child: logo()),
-          Expanded(
-            flex: 5,
-            child: Container(
-              margin: EdgeInsets.fromLTRB(10, 10, 10, 5),
-              padding: EdgeInsets.fromLTRB(20, 0, 20, 0),
-              decoration: BoxDecoration(
-                  color: Colors.white,
-                  borderRadius: BorderRadius.circular(15),
-                  boxShadow: [
-                    BoxShadow(
-                      color: Colors.grey.withOpacity(0.4),
-                      spreadRadius: 0,
-                      blurRadius: 2,
-                      offset: Offset(0, 7), // changes position of shadow
-                    )
-                  ]),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Flexible(
-                    child: TextField(
-                      decoration: InputDecoration(
-                        border: InputBorder.none,
-                        hintText: 'Search Festival',
-                      ),
-                    ),
-                  ),
-                  Flexible(
-                      child: Icon(
-                    Icons.search,
-                    color: Color.fromRGBO(255, 126, 0, 1.0),
-                  ))
-                ],
-              ),
-            ),
-          )
-        ],
-      ),
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: MyAppOne(),
     );
   }
 }
 
-Widget logo() {
-  return Image.asset(
-    'images/mark.jpg',
-  );
+class MyAppOne extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyAppOne> {
+  late KakaoMapController mapController;
+  MapPoint _visibleRegion = MapPoint(37.5087553, 127.0632877);
+  final CameraPosition _kInitialPosition =
+      CameraPosition(target: MapPoint(37.5087553, 127.0632877), zoom: 7);
+
+  void onMapCreated(KakaoMapController controller) async {
+    final MapPoint visibleRegion = await controller.getMapCenterPoint();
+    setState(() {
+      mapController = controller;
+      _visibleRegion = visibleRegion;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Flutter KakaoMap example')),
+      body: Column(
+        children: [
+          Center(
+              child: Container(
+            decoration: BoxDecoration(border: Border.all(color: Colors.black)),
+            child: SizedBox(
+                width: 400.0,
+                height: 300.0,
+                child: KakaoMap(
+                    onMapCreated: onMapCreated,
+                    initialCameraPosition: _kInitialPosition)),
+          ))
+        ],
+      ),
+    );
+  }
 }
