13fd4a8a7d05fbd9563e9985f5648d827f1aee2b
MINSU <ancx1234@naver.com>
2022-08-01 14:25:34 +0900

    민수/web/상단 탑 구성

---
+++ fing-website/src/components/Home.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+export const Home = () => {
+  return (
+    <div className='mainPage'>
+      <h2 id='logo'>Fing</h2>
+      <div className="top">
+        <h2>페스티벌이 궁금할땐?</h2>
+        <h2>Fing 찍어!</h2>
+      </div>
+      
+    </div>
+
+  )
+}
