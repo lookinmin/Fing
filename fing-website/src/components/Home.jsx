b1817c846ae610008bc578ed570fbcf03dfeea2f
MINSU <ancx1234@naver.com>
2022-08-04 23:56:17 +0900

    민수/website/Main페이지 구성
    사진 background, 우측하단 fixed, 문구 추가

--- fing-website/src/components/Home.jsx
+++ fing-website/src/components/Home.jsx
@@ -2,14 +2,51 @@ import React from 'react'
 
 export const Home = () => {
   return (
-    <div className='mainPage'>
-      <h2 id='logo'>Fing</h2>
-      <div className="top">
-        <h2>페스티벌이 궁금할땐?</h2>
-        <h2>Fing 찍어!</h2>
+    <div className="home">
+      <div className='mainPage'>
+        <h2 id='logo'>Fing</h2>
+        <div className="top">
+          <h2>페스티벌이 궁금해?</h2>
+          <h2>Fing 찍어!</h2>
+        </div>
       </div>
+
+      <div className="toDownload">
+        <img src="./img/fingLogo.png" alt="fing" height="70px"/>
+        <h2>Fing Download</h2>
+        <img id='goPS' src="./img/playstore.png" alt="fing" height="30px"/>
+      </div>
+
+      <div className="exOne">
+        <div className="txtBox">
+          <h2>페스티벌이 열리는 모든곳에</h2>
+          <h3>페스티벌에 대한 정보는</h3>
+          <h3><span className='t1'>Fing</span>과 함께</h3>
+        </div>
+
+      </div>
+
       
+      <div className="exOne">
+        <div className="txtBox">
+          <h2>지역 소상공인들을 위한</h2>
+          <h3>상생하는 페스티벌은</h3>
+          <h3><span className='t1'>Fing</span>과 함께</h3>
+        </div>
+
+      </div>
+
+      <div className="exOne">
+        <div className="txtBox">
+          <h2>페스티벌만 보고 갈거야?</h2>
+          <h3>근처 놀거리 먹거리 역시</h3>
+          <h3><span className='t1'>Fing</span>과 함께</h3>
+        </div>
+
+      </div>
+
     </div>
+    
 
   )
 }
