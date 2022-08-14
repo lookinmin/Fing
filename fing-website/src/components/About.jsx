51ab49715cff31a8d02982dc7618298265cfbed7
MINSU <ancx1234@naver.com>
2022-08-14 19:40:52 +0900

    민수/website/about페이지 작성

--- fing-website/src/components/About.jsx
+++ fing-website/src/components/About.jsx
@@ -1,18 +1,99 @@
-import React from 'react';
-import $ from 'jquery';
 import { Menu } from './Menu';
+import React, { useState, useEffect, useRef } from 'react';
+import $ from 'jquery';
+
 
 export const About = () => {
 
+  const [scrollPosition, setScrollPosition] = useState(0);      //현재 스크롤 하는 부분이 어딘지 받아올 State
+  const updateScroll = () => {
+    setScrollPosition(window.scrollY || document.documentElement.scrollTop);
+  }
+  useEffect(()=>{
+      window.addEventListener('scroll', updateScroll);
+  });
+  class Scroll extends React.Component{
+    constructor() {
+      super();
+        this.state = {
+          scrollTop: 0
+        }
+    }
+       
+    componentDidMount = () => {
+      window.addEventListener('scroll', this.handleScroll);
+    }
+    componentWillUnMount = () => {
+      window.removeEventListener('scroll', this.handleScroll);
+    }
+
+    handleScroll = () => {
+      const scrollTop = ('scroll', scrollPosition);
+      this.setState({
+        scrollTop
+      })
+      
+      
+
+
+
+      if(450 > scrollTop && scrollTop > 0){
+        const y1 = $(".men1").offset().top;
+        window.scrollTo({top : y1, behavior: "smooth"})
+        console.log("일단해" + 11)
+      }
+      else if(1500 > scrollTop && scrollTop >= 450){
+        const y2 = $(".men2").offset().top;
+        window.scrollTo({top : y2, behavior: "smooth"})
+        console.log("일단해" + 22)
+      }
+      else if(2700 > scrollTop && scrollTop >= 1500){
+        const y3 = $(".men3").offset().top;
+        window.scrollTo({top : y3, behavior: "smooth"})
+        console.log("일단해" + 33)
+      }
+      else if(scrollTop >= 2700){
+        const y4 = $(".men4").offset().top;
+        window.scrollTo({top : y4, behavior: "smooth"})
+        console.log("일단해" + 44)
+      }
+    }
+  }
+
+  const scr = new Scroll();
+
+  const moveTop = () => {                                   //로고 누르면 최상단으로 이동하는 코드
+    window.scrollTo({top: 0, behavior : "smooth"});
+  }
 
   return (
-    <div className='aboutDiv'>
-      <div className="men1">
+    <div className='aboutDiv' onScroll={scr.handleScroll()}>
+      <h2 id='logo' onClick={() => moveTop()} className="white">Fing</h2>
+      <div className="men1" >
         <Menu/>
+        <div className="aboutTxt" id='target1' >
+          <h2>페스티벌이 있는 모든 곳에</h2>
+          <h2 className='t2'>Fing : Festival - ing</h2>
+        </div>
       </div>
-      <div className="men2"></div>
-      <div className="men3"></div>
-      <div className="men4"></div>
+      <div className="men2" >
+        <div className="aboutTxt" id='target2'>
+          <h2><span className='t2'>Fing</span>은 여러분과 함께 성장합니다.</h2>
+          <h2>더욱 많은 페스티벌을 <span className='t2'>Fing</span>에 담겠습니다.</h2>
+        </div>
+      </div>
+      <div className="men3">
+        <div className="aboutTxt" id='target3'>
+          <h2>당신의 페스티벌?<span className='t2'> Fing</span>과 함께</h2>
+          <h2>페스티벌 공유 & 홍보 역시 <span className='t2'> Fing</span></h2>
+        </div>
+      </div>
+      <div className="men4" >
+        <div className="aboutTxt" id='target4'>
+          <h2><span className='t2'>Fing</span>은 Tour API 4.0과 함께 합니다.</h2>
+          <h2>페스티벌이 있는 곳엔? 역시 <span className='t2'>Fing</span></h2>
+        </div>
+      </div>
     </div>
   )
 }
--- fing-website/src/components/Home.jsx
+++ fing-website/src/components/Home.jsx
@@ -1,9 +1,8 @@
-import React from 'react'
 import { Footer } from './Footer'
 import { Menu } from './Menu'
 import { MdRoom } from 'react-icons/md';
 import styles from './Home.module.css';
-import { useRef, useState, useEffect } from 'react';
+import React, { useRef, useState, useEffect } from 'react';
 import { useIntersectionObserver } from './useIntersectionObserver.ts';
 import { IoAnalyticsOutline,IoPartlySunnyOutline, IoHeartOutline } from "react-icons/io5";
 
@@ -11,31 +10,38 @@ import { IoAnalyticsOutline,IoPartlySunnyOutline, IoHeartOutline } from "react-i
 
 export const Home = () => {
 
-  const [scrollPosition, setScrollPosition] = useState(0);
+
+
+  const [scrollPosition, setScrollPosition] = useState(0);      //현재 스크롤 하는 부분이 어딘지 받아올 State
   const updateScroll = () => {
-      setScrollPosition(window.scrollY || document.documentElement.scrollTop);
+    setScrollPosition(window.scrollY || document.documentElement.scrollTop);
   }
   useEffect(()=>{
       window.addEventListener('scroll', updateScroll);
   });
 
-  const moveTop = () => {
+  const moveTop = () => {                                   //로고 누르면 최상단으로 이동하는 코드
     window.scrollTo({top: 0, behavior : "smooth"});
   }
    
-
-
-
 
   const target1 = useRef(null)
   const target2 = useRef(null)
   const target3 = useRef(null)
   const target4 = useRef(null)
 
+  const img1 = useRef(null)
+  const img2 = useRef(null)
+  const img3 = useRef(null)
+                                                                  //스크롤 구간에 맞춰 Fing 글자 Fade IN
   useIntersectionObserver(target1, styles.animation);
   useIntersectionObserver(target2, styles.animation);
   useIntersectionObserver(target3, styles.animation);
   useIntersectionObserver(target4, styles.animation);
+
+  useIntersectionObserver(img1, styles.imgAni);
+  useIntersectionObserver(img2, styles.imgAni);
+  useIntersectionObserver(img3, styles.imgAni);
 
   return (
     <div className="home">
@@ -57,7 +63,7 @@ export const Home = () => {
 
       <div className="txtsBox">
         <h2 id='y1'>페스티벌은?</h2>
-        <h2 id='y2'>Let's <span className='t1' ref={target1}>Fing</span><MdRoom size="50" color= 'rgb(255, 126, 0)'/></h2>
+        <h2 id='y2'>Let's <span className='t1' ref={target1}>Fing<MdRoom size="60" color= 'rgb(255, 126, 0)'/></span></h2>
       </div>
 
       <div className="itemsList">
@@ -65,19 +71,19 @@ export const Home = () => {
           <div className="box">
             <span className='icons'><IoPartlySunnyOutline size="50"/></span>
           </div>
-          <h2><span className='t1'>계절</span>별로 즐기는 페스티벌</h2>
+          <h2><span className='t2'>계절</span>별로 즐기는 페스티벌</h2>
         </div>
         <div className="items">
           <div className="box">
             <span className='icons'><IoHeartOutline size="50"/></span>
           </div>
-          <h2>관심있는 페스티벌을 <span className='t1'>찜</span></h2>
+          <h2>관심있는 페스티벌을 <span className='t2'>찜</span></h2>
         </div>
         <div className="items">
           <div className="box">
             <span className='icons'><IoAnalyticsOutline size="50"/></span>
           </div>
-          <h2><span className='t1'>Hot</span>한 페스티벌은?</h2>
+          <h2><span className='t2'>Hot</span>한 페스티벌은?</h2>
         </div>
       </div>
 
@@ -87,7 +93,7 @@ export const Home = () => {
           <h3>페스티벌에 대한 정보는</h3>
           <h3><span className='t1' ref={target2}>Fing</span>과 함께</h3>
         </div>
-
+        <img className='homeImg' ref={img1} src="./img/pic1.jpg" height="500px" alt="" />
       </div>
 
       
@@ -97,7 +103,7 @@ export const Home = () => {
           <h3>상생하는 페스티벌은</h3>
           <h3><span className='t1' ref={target3}>Fing</span>과 함께</h3>
         </div>
-
+        <img className='homeImg' ref={img2} src="./img/pic2.jpg" height="500px" alt="" />
       </div>
 
       <div className="exOne">
@@ -106,7 +112,7 @@ export const Home = () => {
           <h3>근처 놀거리 먹거리 역시</h3>
           <h3><span className='t1' ref={target4}>Fing</span>과 함께</h3>
         </div>
-
+        <img className='homeImg' ref={img3} src="./img/pic3.jpg" height="500px" alt="" />
       </div>
 
 
