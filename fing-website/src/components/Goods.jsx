c92043643ea14b3d9dbe6a4ce927774c8b8e085f
MINSU <ancx1234@naver.com>
2022-08-19 20:46:27 +0900

    민수/web/about 3번째 사진 변경

--- fing-website/src/components/Goods.jsx
+++ fing-website/src/components/Goods.jsx
@@ -6,7 +6,7 @@ export const Goods = () => {
   return (
     <div className="goods">
       <div className="h2">아니 시비라 어띠</div>
-      <h2 id='logo' className='white'>Fing</h2>
+      <h2 id='logo' className='white'>Fing Market</h2>
       <Menu/>
 
       <div className="toDownload">
