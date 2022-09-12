461321016d26933d9916d13cb71b5e5fe20e5d7a
kkm0406 <sdjmc30412@naver.com>
2022-09-12 21:22:00 +0900

    develop-website

--- fing-website/src/components/Menu.jsx
+++ fing-website/src/components/Menu.jsx
@@ -1,12 +1,26 @@
 import React from 'react'
+import { useNavigate } from 'react-router-dom';
 
 export const Menu = () => {
+
+  let navigate = useNavigate();
+  function handleClick(e) {
+    if(e===1){
+      navigate("/");
+    }
+    else if(e===2){
+      navigate("/goods");
+    }
+    else{
+      navigate("/service");
+    }
+  }
   return (
     <div className="menu">
       <ul>
-        <li><a href="/">home</a></li>
-        <li><a href="/goods">goods</a></li>
-        <li><a href="/about">about</a></li>
+        <li><a style={{"cursor": "pointer"}} onClick={() => handleClick(1)}>home</a></li>
+        <li><a style={{"cursor": "pointer"}} onClick={() => handleClick(2)}>goods</a></li>
+        <li><a style={{"cursor": "pointer"}} onClick={() => handleClick(3)}>about</a></li>
       </ul>
     </div>
   )
