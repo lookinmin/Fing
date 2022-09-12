2f8acaff801a47ad61f4d6928108477e60e96438
MINSU <ancx1234@naver.com>
2022-09-12 20:51:40 +0900

    민수/main/version Up

--- fing/macos/Flutter/GeneratedPluginRegistrant.swift
+++ fing/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -13,6 +13,7 @@ import location
 import package_info_plus_macos
 import path_provider_macos
 import shared_preferences_macos
+import sqflite
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FLTFirebaseFirestorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseFirestorePlugin"))
@@ -23,4 +24,5 @@ func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FLTPackageInfoPlusPlugin.register(with: registry.registrar(forPlugin: "FLTPackageInfoPlusPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
+  SqflitePlugin.register(with: registry.registrar(forPlugin: "SqflitePlugin"))
 }
