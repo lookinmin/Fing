6da1176df2c8a0d2d79c26c079904102bec392b0
Jo MINSU <50093223+lookinmin@users.noreply.github.com>
2023-11-29 18:46:50 +0900

    랜딩 웹페이지 menu 변경

--- fing-website/src/components/Menu.jsx
+++ fing-website/src/components/Menu.jsx
@@ -1,13 +1,22 @@
-import React from 'react'
+import React from "react";
+import { useNavigate } from "react-router-dom";
 
 export const Menu = () => {
+  const navigate = useNavigate();
+
   return (
     <div className="menu">
       <ul>
-        <li><a href="/">home</a></li>
-        <li><a href="/goods">goods</a></li>
-        <li><a href="/about">about</a></li>
+        <li style={{ cursor: "pointer" }}>
+          <a onClick={() => navigate("/")}>home</a>
+        </li>
+        <li style={{ cursor: "pointer" }}>
+          <a onClick={() => navigate("/goods")}>goods</a>
+        </li>
+        <li style={{ cursor: "pointer" }}>
+          <a onClick={() => navigate("/about")}>about</a>
+        </li>
       </ul>
     </div>
-  )
-}
+  );
+};
